8c4148bf58dc02df3622ea8c0ad53a2f41d52047
brasilhenrique <brasilhenrique@gmail.com>
2025-07-10 09:39:36 -0300

    Update production.py

--- sigaf_project/settings/production.py
+++ sigaf_project/settings/production.py
@@ -8,8 +8,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'uma-chave-padrao-de-seguranca-caso-a-variavel-nao-esteja-setada')
 
-# --- ALTERAÇÃO AQUI ---
-# Adicionamos os nomes de domínio à lista de hosts permitidos
+# Hosts permitidos
 ALLOWED_HOSTS = ['henriquebrasil.com.br', 'www.henriquebrasil.com.br', '167.172.137.159']
 
 # Configuração do banco de dados PostgreSQL
@@ -18,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sigaf_db',
         'USER': 'sigaf_user',
-        'PASSWORD': 'a_senha_forte_que_voce_anotou', # <-- ATENÇÃO: COLOQUE A SENHA QUE VOCÊ CRIOU AQUI
+        'PASSWORD': 'a_senha_forte_que_voce_anotou', # <-- ATENÇÃO: COLOQUE A SENHA CORRETA AQUI
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -32,3 +31,10 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+# --- CONFIGURAÇÃO DO LOGOFF AUTOMÁTICO POR INATIVIDADE ---
+# Tempo da sessão em segundos (60 minutos * 60 segundos = 3600)
+SESSION_COOKIE_AGE = 3600
+
+# Salva a sessão a cada requisição, resetando o timer de inatividade
+SESSION_SAVE_EVERY_REQUEST = True
